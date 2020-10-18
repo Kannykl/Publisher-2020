4ef4153a82ffe1b2afb1fad0af1f4dd1c1c9aed4
Kanny <kanny@mail.ru>
2020-10-18 15:35:05 +0300

    Сделал страницу добавления новых записей в таблицу

--- publisher/publications_table/urls.py
+++ publisher/publications_table/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import show_all_publications
-
+from .views import (
+    show_all_publications,
+    PublicationCreateView
+)
 
 urlpatterns = [
     path('all_publications/', show_all_publications, name='all'),
+    path('create_publication/', PublicationCreateView.as_view(), name='publication-create'),
 ]
--- publisher/publications_table/views.py
+++ publisher/publications_table/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from .models import Publication
+from django.views.generic import CreateView
 
 
 def show_all_publications(request):
+    """ Страница отображения всех записей"""
     publications = Publication.objects.all()
     context = {
         'publications': publications
     }
     return render(request, "publications_table/all_publications.html", context)
+
+
+class PublicationCreateView(CreateView):
+    """ Страница создания новой записи"""
+    model = Publication
+    template_name = 'publications_table/publication_create.html'
+    fields = '__all__'
+    success_url = '/all_publications/'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
