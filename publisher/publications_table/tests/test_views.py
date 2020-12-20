a885a92f71b9084ae75d8b33c58a0839821d8968
Kanny <kanny@mail.ru>
2020-12-21 02:15:02 +0300

    Update

--- publisher/publications_table/tests/test_views.py
+++ publisher/publications_table/tests/test_views.py
@@ -1,6 +1,5 @@
 import pytest
 from django.contrib.auth.models import AnonymousUser
-from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.test import RequestFactory
 from django.test import TestCase
