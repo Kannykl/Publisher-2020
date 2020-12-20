6d94a8c8e34f0dc706c962b77ee7b7dfd400932b
Kanny <kanny@mail.ru>
2020-12-20 12:53:17 +0300

    Протестировано удачное создание автора, публикации и перехода на главную страницу

--- publisher/publications_table/tests/test_views.py
+++ publisher/publications_table/tests/test_views.py
@@ -1,13 +1,60 @@
 import pytest
+from django.contrib.auth.models import AnonymousUser
 from django.urls import reverse
 from django.test import RequestFactory
 from django.test import TestCase
 from mixer.backend.django import mixer
-from publications_table.models import Publication
+from publications_table.models import Publication, Author
+from publications_table.views import show_all_publications, AuthorCreateView, create_publication
 
 
 @pytest.mark.django_db
 class TestViews(TestCase):
+
+    def test_show_all_publications(self):
+        path = reverse('all')
+        request = RequestFactory().get(path)
+        request.user = AnonymousUser()
+        response = show_all_publications(request)
+        assert response.status_code == 200
+
+    def test_create_author(self):
+        assert list(Author.objects.all()) == list(Author.objects.none())
+        path = reverse('create-author')
+        request = RequestFactory().post(path, data={
+            'name': 'TestName',
+            'surname': 'TestSurname',
+            'patronymic': 'TestPatronymic',
+            'work_position': 'TestWorkPosition',
+            'military_rank': 'TestMilitaryRank',
+        })
+        request.user = AnonymousUser()
+        response = AuthorCreateView.as_view()(request)
+        assert response.status_code == 302
+        assert '/publisher/create_publication/' in response.url
+        assert Author.objects.get(pk=1).name == 'TestName'
+        assert Author.objects.count() == 1
+
+    def test_create_publication(self):
+        assert Publication.objects.count() == 0
+        mixer.blend('publications_table.Author', name='TestName', surname='TestSurname',
+                    patronymic='TestPatronymic', work_position='TestWorkPosition', military_rank='TestMilitaryRank')
+        path = reverse('publication-create')
+        request = RequestFactory().post(path, data={
+            'authors': Author.objects.get(name='TestName').id,
+            'title': 'Статья1',
+            'published_year': 2020,
+            'type_of_publication': 'Тезис',
+            'edition': 'издание',
+            'range': '1-2',
+            'uk_number': 2441
+        })
+        request.user = AnonymousUser()
+        response = create_publication(request)
+        assert response.status_code == 302
+        assert 'publisher/' in response.url
+        assert Publication.objects.count() == 1
+        assert Publication.objects.all()[0].title == 'Статья1'
 
     def test_delete_publication(self):
         publication = mixer.blend('publications_table.Publication', title='Статья1', published_year='2020',
