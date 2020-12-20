5e2fadda748348848bb02f7395a1257b3a6103f7
Kanny <kanny@mail.ru>
2020-12-20 13:08:00 +0300

    Протестировано успешное редактирование статьи

--- publisher/publications_table/tests/test_views.py
+++ publisher/publications_table/tests/test_views.py
@@ -5,7 +5,7 @@ from django.test import RequestFactory
 from django.test import TestCase
 from mixer.backend.django import mixer
 from publications_table.models import Publication, Author
-from publications_table.views import show_all_publications, AuthorCreateView, create_publication
+from publications_table.views import show_all_publications, AuthorCreateView, create_publication, update_publication
 
 
 @pytest.mark.django_db
@@ -69,3 +69,27 @@ class TestViews(TestCase):
         assert response.status_code == 302
         assert '/publisher/' in response.url
         assert Publication.objects.count() == 0
+
+    def test_success_update_publication(self):
+        """Успешное редактирование публикации"""
+        assert Publication.objects.count() == 0
+        mixer.blend('publications_table.Author', name='TestName', surname='TestSurname',
+                    patronymic='TestPatronymic', work_position='TestWorkPosition', military_rank='TestMilitaryRank')
+        publication = mixer.blend('publications_table.Publication', title='СтатьяДоИзменения', published_year='2020',
+                                  type_of_publication='Тезис', edition='издание', range='1-2', uk_number='123')
+        path = reverse('publication-update', kwargs={'pk': publication.id})
+        request = RequestFactory().post(path, data={
+            'authors': Author.objects.get(name='TestName').id,
+            'title': 'СтатьяПослеИзменения',
+            'published_year': 2020,
+            'type_of_publication': 'Тезис',
+            'edition': 'издание',
+            'range': '1-2',
+            'uk_number': 2441
+        })
+        request.user = AnonymousUser()
+        response = update_publication(request, pk=publication.id)
+        assert response.status_code == 302
+        assert f'/publisher/publication_info/{publication.id}' in response.url
+        assert Publication.objects.count() == 1
+        assert Publication.objects.all()[0].title == 'СтатьяПослеИзменения'
