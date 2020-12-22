bc986629a22ca59b9136a078a8704159d456a9a4
Kanny <kanny@mail.ru>
2020-12-22 10:32:06 +0300

    изменен url статики в настройках проекта и в файле конфигурации

--- publisher/settings.py
+++ publisher/settings.py
@@ -62,7 +62,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+STATIC_URL = '/static/publisher/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES = [
