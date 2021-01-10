a46a38ccaea3c93487cdf508e88dd7b5789a146b
kanny <kanny@mail.ru>
2021-01-10 13:35:43 +0300

    Изменена функция экспорта таблицы, добавлена проверка расширения файла

--- publications_table/export.py
+++ publications_table/export.py
@@ -4,10 +4,19 @@ from django.conf import settings
 from .models import Table
 
 
+def check_extension(file_name):
+    extension = file_name.split('.')[-1]
+    if extension == 'xls':
+        return file_name
+    else:
+        return file_name+'.xls'
+
+
 def export_in_xls(publications, file_name):
+    file_name = check_extension(file_name)
     workbook = xlsxwriter.Workbook(os.path.join(settings.MEDIA_ROOT, f'{file_name}'))
     worksheet = workbook.add_worksheet()
-    headers = ['Звание', 'ФИО', 'Должность', 'Название', 'Издание', 'Год публикации', 'Тип публикации',
+    headers = ['Название', 'Издание', 'Год публикации', 'Тип публикации',
                'Диапазон', 'Номер УК']
 
     col = 0
@@ -17,19 +26,14 @@ def export_in_xls(publications, file_name):
 
     row = 1
     for publication in publications:
-        for author in publication.authors.all():
-            col = 0
-            worksheet.write(row, col, author.military_rank)
-            initials = author.surname + ' ' + author.name[:1] + '.' + author.patronymic[:1] + '.'
-            worksheet.write(row, col + 1, initials)
-            worksheet.write(row, col + 2, author.work_position)
-            worksheet.write(row, col + 3, publication.title)
-            worksheet.write(row, col + 4, publication.edition)
-            worksheet.write(row, col + 5, publication.published_year)
-            worksheet.write(row, col + 6, publication.type_of_publication)
-            worksheet.write(row, col + 7, publication.range)
-            worksheet.write(row, col + 8, publication.uk_number)
-            row += 1
+        col = 0
+        worksheet.write(row, col, publication.title)
+        worksheet.write(row, col + 1, publication.edition)
+        worksheet.write(row, col + 2, publication.published_year)
+        worksheet.write(row, col + 3, publication.type_of_publication.type_of_publication)
+        worksheet.write(row, col + 4, publication.range)
+        worksheet.write(row, col + 5, publication.uk_number)
+        row += 1
     workbook.close()
     file = Table.objects.create(file=f'{file_name}')
     return file
